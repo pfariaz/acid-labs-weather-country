d0162ad3f6d025854ad60ac0d6a890f0cffc3047
pfariaz <pfariaz011@gmail.com>
2018-11-25 21:43:31 -0300

    fix path

--- client/src/services/weatherForecast.js
+++ client/src/services/weatherForecast.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const getWeatherByCoordinates = (longitude, latitude) =>
-    axios.get(`/api/api/v1/weather?lat=${latitude}&long=${longitude}`)
+    axios.get(`/api/v1/weather?lat=${latitude}&long=${longitude}`)
     .then(response => response.data);
 
 export {
